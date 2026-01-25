f347dfb1d6772bf7cce5489a3bcdd4fb7875a8fc
deepak <d9111pak@gmail.com>
2026-01-25 11:33:16 +0530

    fix model for notification

--- src/models/notification.model.js
+++ src/models/notification.model.js
@@ -15,43 +15,36 @@ const notificationSchema = new Schema(
       index: true,
     },
     type: {
-      type : String,
-      enum: [
-        "like",
-        "comment",
-        "reply",
-        "repost",
-        "follow",
-        "mention",
-      ],
+      type: String,
+      enum: ["like", "comment", "reply", "repost", "follow", "mention"],
       required: true,
       index: true,
     },
-    post : {
-      type : Schema.Types.ObjectId,
-      ref: "Post",
-      default : null
+    entityId: {
+      type: Schema.Types.ObjectId,
+      required: true,
     },
-    comment : {
-      type : Schema.Types.ObjectId,
-      ref: "Post",
-      default : null
+    entityType: {
+      type: String,
+      enum: ["Tweet", "Comment"],
+      required: true,
     },
-    isRead : {
+    isRead: {
       type: Boolean,
       default: false,
       index: true,
-    }
+    },
   },
   { timestamps: true }
 );
-//Optimise feed ke liye 
-notificationSchema.index({receiver: 1, createdAt: -1})
-//New notify ke liye 
-notificationSchema.index({receiver: 1, isRead: 1})
+
+notificationSchema.index({ receiver: 1, createdAt: -1 });
+notificationSchema.index({ receiver: 1, isRead: 1 });
+
 notificationSchema.index(
-  { sender: 1, receiver: 1, type: 1, post: 1 },
-  { unique: false }
+  { sender: 1, receiver: 1, type: 1, entityId: 1 },
+  { unique: true, sparse: true }
 );
+
 
 export const Notification = mongoose.model("Notification", notificationSchema);
